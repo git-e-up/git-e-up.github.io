51e848f45e2120914412317cf2481c33467c6f19
Matt Lissner <mlissne@gmail.com>
2015-06-29 12:07:02 -0500

    unknown

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -44,6 +44,15 @@ $(document).ready(function () {
 
         setInterval(pic, 6000);
 
-
+        // var resize = function(){
+        //   console.log('resize called');
+        //   var width = $(window).width();
+        //   if(width <= 500){
+        //     $('#video-sec').removeID('#video-sec')add;
+        //   }
+        //
+        //
+        // };
+        // resize //trigger the resize event on page load.
 
 });
