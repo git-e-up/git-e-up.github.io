077d36dcbe98f926f4094e6946252b8620884480
Matt Lissner <mlissne@gmail.com>
2015-06-30 01:10:05 -0500

    comment out js

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -44,20 +44,20 @@ $(document).ready(function () {
 
         setInterval(pic, 6000);
 
-        var resize = function() {
-          console.log('resize called');
-          var width = $(window).width();
-          if(width <= 500){
-            $('#home').addClass(".player2");
-          };
-
-
-        };
-        resize(); //trigger the resize event on page load.
+        // var resize = function() {
+        //   console.log('resize called');
+        //   var width = $(window).width();
+        //   if(width <= 500){
+        //     $('#home').addClass("player2");
+        //   };
+        //
+        //
+        // };
+        // resize(); //trigger the resize event on page load.
         /////////////////
 
         // $( window ).resize(function() {
-        //   $( "#video-sec").addClass("player2");
+        //   $( "#home").addClass("player2");
         // });
 
 });
